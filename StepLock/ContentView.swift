442e2240094a345c5b2d232e2200dc7ea08ead72
JJ <JJ@gmail.com>
2026-04-29 15:55:13 +1000

    Initial Commit

---
+++ StepLock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StepLock
+//
+//  Created by JJ on 29/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StepLock/StepLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepLockApp.swift
+//  StepLock
+//
+//  Created by JJ on 29/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct StepLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
